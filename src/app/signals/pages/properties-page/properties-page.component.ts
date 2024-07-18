f70115e8be4e2c1611477ae969fc4d1c52580fec
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-07-18 11:13:45 -0500

    clase 333: estructura de pantalla

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css'
 })
+
 export class PropertiesPageComponent {
+
+  onFieldUpdated(field: string, value: string){
+    console.log(field, value);
+  }
 
 }

70aa525aa596915769a3f7507c9b5f12718d49b4
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-07-17 21:04:29 -0500

    clase 331: señales - info usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UserServiceService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
 
+export class UserInfoPageComponent implements OnInit{
+  private usersService = inject(UserServiceService);
+  public userId = signal(1);
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number): void{
+    if(id <= 0) return;
+    this.userId.set(id)
+    this.usersService.getUserById(id).subscribe({
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false);
+      }
+    });
+  }
 }
